367d24c6275dd36765bb4de9b36ea2ff83939824
FelAlmanzar <almanzar769@gmail.com>
2023-07-20 21:35:19 -0500

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import './App.css';
+
+function App() {
+  return (
+  <main className="main">
+    <Navbar/>
+  </main>
+);
+}
+
+
+export default App
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function Navbar(){
+    return <nav className="navBar">
+        <ul>
+            <li><a href="">Inicio</a></li>
+            <li><a href="">Nosotros</a></li>
+            <li><a href="">Productos</a></li>
+            <li><a href="">Contacto</a></li>
+        </ul>
+    </nav>;
+}
+
+export default Navbar;
